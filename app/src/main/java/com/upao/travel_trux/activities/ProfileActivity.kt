7f3519593e913058da0e6c8646c1b7cfc5862629
Alejandro Escobedo Correa <toshiro.escobedo@intediperu.com>
2024-05-19 20:46:43 -0500

    fix error

--- app/src/main/java/com/upao/travel_trux/activities/ProfileActivity.kt
+++ app/src/main/java/com/upao/travel_trux/activities/ProfileActivity.kt
@@ -1,24 +1,57 @@
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.view.Menu
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.upao.travel_trux.R
-import com.upao.travel_trux.activities.EditarActivity
+import com.upao.travel_trux.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
-    lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT) // light causes internally enforce the navigation bar to be fully transparent
         )
-        setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarProfile.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_profile)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_profile
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.profile, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_profile)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
